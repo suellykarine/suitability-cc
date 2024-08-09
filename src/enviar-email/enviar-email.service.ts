5f95518ed3100607637b81c8c05c86548001ccd9
lorenzo.consultoria+1-at-887626648304 <lorenzo.consultoria@wefin.com.br>
2024-08-09 14:38:35 -0300

    refatoramento:enviar-email-service

--- src/enviar-email/enviar-email.service.ts
+++ src/enviar-email/enviar-email.service.ts
@@ -2,10 +2,11 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
   ServiceUnavailableException,
 } from '@nestjs/common';
 import { EnviarEmailDto } from './dto/create-enviar-email.dto';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, usuario } from '@prisma/client';
 import { SolicitacaoBase } from 'src/utils/interfaces/solicitacaoBase.interface';
 import { jwtConstants } from 'src/auth/constants';
 import * as jwt from 'jsonwebtoken';
@@ -17,104 +18,21 @@ export class EnviarEmailService {
   constructor() {
     this.prisma = new PrismaClient();
   }
+
   async enviarEmail(enviarEmailDto: EnviarEmailDto) {
-    const usuario = await this.prisma.usuario.findUnique({
-      where: {
-        email: enviarEmailDto.para,
-      },
-    });
-
+    const usuario = await this.obterUsuarioPorEmail(enviarEmailDto.para);
     const nomeUsuario = usuario ? usuario.nome : 'Investidor';
-    const solicitacaoBase: SolicitacaoBase = {
-      contentParam: {
-        nome: nomeUsuario!,
-      },
-      mail: {
-        addressesCcTo: [],
-        addressesTo: <string[]>[enviarEmailDto.para],
-        emailFrom: 'srmasset@srmasset.com.br',
-        subject: enviarEmailDto.tituloDaMensagem,
-      },
-      templateName: '',
-    };
 
-    switch (enviarEmailDto.tipo) {
-      case 'validado':
-        solicitacaoBase.contentParam.urlPlataforma =
-          process.env.URL_DA_PLATAFORMA;
-        solicitacaoBase.templateName =
-          'credit_connect_documentacao_usuario_aprovada.html';
-        break;
+    const solicitacaoBase: SolicitacaoBase = this.criarSolicitacaoBase(
+      enviarEmailDto,
+      nomeUsuario,
+    );
 
-      case 'reprovado':
-        solicitacaoBase.templateName =
-          'credit_connect_usuario_trial_reprovado.html';
-        break;
-
-      case 'aprovado':
-        const cartaConvite = await this.prisma.carta_convite.findFirst({
-          where: {
-            email: enviarEmailDto.para,
-          },
-        });
-        const token = jwt.sign(
-          { email: enviarEmailDto.para, id: cartaConvite?.id },
-          jwtConstants.sercretPreRegister,
-          {
-            expiresIn: '30d',
-          },
-        );
-        const url = `${process.env.URL_DA_PLATAFORMA}pre-register/${token}`;
-        solicitacaoBase.contentParam.urlPlataforma = url;
-        solicitacaoBase.templateName =
-          'credit_connect_usuario_trial_aprovado.html';
-        break;
-
-      case 'enviarDocBackOffice':
-        const backOffice = await this.prisma.fundo_backoffice.findUnique({
-          where: {
-            email: enviarEmailDto.para,
-          },
-        });
-        const tokenBackoffice = this.gerarTokenBackoffice(
-          backOffice!.id,
-          30,
-          usuario?.id,
-          usuario?.email!,
-        );
-        const urlBackoffice = `${process.env.URL_DA_PLATAFORMA}/backoffice/${tokenBackoffice}`;
-        solicitacaoBase.contentParam.nomeDoFundo = enviarEmailDto.nomeDoFundo;
-        solicitacaoBase.contentParam.urlDocumentosBackoffice = urlBackoffice;
-        solicitacaoBase.templateName =
-          'credit_connect_email_para_backoffice_enviar_documentos_do_fundo.html';
-        break;
-
-      case 'assinaturaPendente':
-        if (!enviarEmailDto.docsToSign) {
-          throw new BadRequestException({
-            mensagem: 'O Campo docsToSign não pode estar vazio',
-          });
-        }
-        solicitacaoBase.contentParam.docusign = enviarEmailDto.docsToSign;
-        solicitacaoBase.templateName =
-          'credit_connect_assinatura_pendente.html';
-        break;
-
-      case 'redefinirSenha':
-        if (!enviarEmailDto.docsToSign) {
-          throw new BadRequestException({
-            mensagem: 'O Campo docsToSign não pode estar vazio',
-          });
-        }
-        solicitacaoBase.contentParam.novaSenha = enviarEmailDto.docsToSign;
-        solicitacaoBase.templateName = 'credit_connect_redefinir_senha.html';
-        break;
-
-      default:
-        throw new BadRequestException({
-          mensage: 'Tipo de email não reconhecido',
-        });
-    }
+    await this.prepararSolicitacaoBase(
+      solicitacaoBase,
+      enviarEmailDto,
+      usuario,
+    );
 
     const resposta = await this.serviceEmailSrm(solicitacaoBase);
     if (!resposta) {
@@ -125,6 +43,158 @@ export class EnviarEmailService {
     return {
       mensagem: 'Email enviado com sucesso',
     };
+  }
+
+  private async obterUsuarioPorEmail(email: string) {
+    return this.prisma.usuario.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  private criarSolicitacaoBase(
+    enviarEmailDto: EnviarEmailDto,
+    nomeUsuario: string,
+  ): SolicitacaoBase {
+    return {
+      contentParam: {
+        nome: nomeUsuario,
+      },
+      mail: {
+        addressesCcTo: [],
+        addressesTo: <string[]>[enviarEmailDto.para],
+        emailFrom: 'srmasset@srmasset.com.br',
+        subject: enviarEmailDto.tituloDaMensagem,
+      },
+      templateName: '',
+    };
+  }
+
+  private async prepararSolicitacaoBase(
+    solicitacaoBase: SolicitacaoBase,
+    enviarEmailDto: EnviarEmailDto,
+    usuario,
+  ) {
+    switch (enviarEmailDto.tipo) {
+      case 'validado':
+        this.configurarTemplateValidado(solicitacaoBase);
+        break;
+
+      case 'reprovado':
+        this.configurarTemplateReprovado(solicitacaoBase);
+        break;
+
+      case 'aprovado':
+        await this.configurarTemplateAprovado(solicitacaoBase, enviarEmailDto);
+        break;
+
+      case 'enviarDocBackOffice':
+        await this.configurarTemplateEnviarDocBackOffice(
+          solicitacaoBase,
+          enviarEmailDto,
+          usuario,
+        );
+        break;
+
+      case 'assinaturaPendente':
+        this.validarCampoObrigatorio(enviarEmailDto.docsToSign, 'docsToSign');
+        this.configurarTemplateAssinaturaPendente(
+          solicitacaoBase,
+          enviarEmailDto,
+        );
+        break;
+
+      case 'redefinirSenha':
+        this.validarCampoObrigatorio(enviarEmailDto.docsToSign, 'docsToSign');
+        this.configurarTemplateRedefinirSenha(solicitacaoBase, enviarEmailDto);
+        break;
+
+      default:
+        throw new BadRequestException({
+          mensage: 'Tipo de email não reconhecido',
+        });
+    }
+  }
+
+  private configurarTemplateValidado(solicitacaoBase: SolicitacaoBase) {
+    solicitacaoBase.contentParam.urlPlataforma = process.env.URL_DA_PLATAFORMA;
+    solicitacaoBase.templateName =
+      'credit_connect_documentacao_usuario_aprovada.html';
+  }
+
+  private configurarTemplateReprovado(solicitacaoBase: SolicitacaoBase) {
+    solicitacaoBase.templateName =
+      'credit_connect_usuario_trial_reprovado.html';
+  }
+
+  private async configurarTemplateAprovado(
+    solicitacaoBase: SolicitacaoBase,
+    enviarEmailDto: EnviarEmailDto,
+  ) {
+    const cartaConvite = await this.prisma.carta_convite.findFirst({
+      where: {
+        email: enviarEmailDto.para,
+      },
+    });
+    const token = jwt.sign(
+      { email: enviarEmailDto.para, id: cartaConvite?.id },
+      jwtConstants.sercretPreRegister,
+      { expiresIn: '30d' },
+    );
+    solicitacaoBase.contentParam.urlPlataforma = `${process.env.URL_DA_PLATAFORMA}pre-register/${token}`;
+    solicitacaoBase.templateName = 'credit_connect_usuario_trial_aprovado.html';
+  }
+
+  private async configurarTemplateEnviarDocBackOffice(
+    solicitacaoBase: SolicitacaoBase,
+    enviarEmailDto: EnviarEmailDto,
+    usuario,
+  ) {
+    const backOffice = await this.prisma.fundo_backoffice.findUnique({
+      where: {
+        email: enviarEmailDto.para,
+      },
+    });
+
+    if (!backOffice) {
+      throw new NotFoundException({ mensagem: 'Backoffice não encontrado' });
+    }
+
+    const tokenBackoffice = this.gerarTokenBackoffice(
+      backOffice!.id,
+      30,
+      usuario?.id,
+      usuario?.email!,
+    );
+    solicitacaoBase.contentParam.nomeDoFundo = enviarEmailDto.nomeDoFundo;
+    solicitacaoBase.contentParam.urlDocumentosBackoffice = `${process.env.URL_DA_PLATAFORMA}backoffice/${tokenBackoffice}`;
+    solicitacaoBase.templateName =
+      'credit_connect_email_para_backoffice_enviar_documentos_do_fundo.html';
+  }
+
+  private configurarTemplateAssinaturaPendente(
+    solicitacaoBase: SolicitacaoBase,
+    enviarEmailDto: EnviarEmailDto,
+  ) {
+    solicitacaoBase.contentParam.docusign = enviarEmailDto.docsToSign;
+    solicitacaoBase.templateName = 'credit_connect_assinatura_pendente.html';
+  }
+
+  private configurarTemplateRedefinirSenha(
+    solicitacaoBase: SolicitacaoBase,
+    enviarEmailDto: EnviarEmailDto,
+  ) {
+    solicitacaoBase.contentParam.novaSenha = enviarEmailDto.docsToSign;
+    solicitacaoBase.templateName = 'credit_connect_redefinir_senha.html';
+  }
+
+  private validarCampoObrigatorio(campo: any, nomeCampo: string) {
+    if (!campo) {
+      throw new BadRequestException({
+        mensagem: `O Campo ${nomeCampo} não pode estar vazio`,
+      });
+    }
   }
 
   private gerarTokenBackoffice(
