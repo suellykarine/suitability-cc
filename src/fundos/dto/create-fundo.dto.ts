9803c4c96a92ffbb34914cecc2e91d7c8983287e
lorenzo.consultoria+1-at-887626648304 <lorenzo.consultoria@wefin.com.br>
2024-08-13 09:16:42 -0300

    fix/fundosSwagger

--- src/fundos/dto/create-fundo.dto.ts
+++ src/fundos/dto/create-fundo.dto.ts
@@ -9,13 +9,7 @@ import {
   ValidateNested,
 } from 'class-validator';
 import { Transform, Type } from 'class-transformer';
-
-export class CreateFundosDto {
-  @IsArray()
-  @ValidateNested({ each: true })
-  @Type(() => CreateFundoDto)
-  fundos: CreateFundoDto[];
-}
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFundoDto {
   @IsString()
@@ -145,4 +139,49 @@ export class CreateFundoDto {
   @IsNumberString()
   @IsOptional()
   faturamento_anual?: string;
+}
+export class CreateFundosDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateFundoDto)
+  @ApiProperty({
+    type: [CreateFundoDto],
+    description: 'Array de fundos para serem criados',
+    example: [
+      {
+        nome: 'Fundo Alpha',
+        razao_social: 'Fundo Alpha S.A.',
+        nome_fantasia: 'Alpha',
+        cpf_cnpj: '12345678000195',
+        codigo_anbima: 'A123',
+        classe_anbima: 'Ações',
+        atividade_principal: 'Investimentos em ações',
+        detalhes:
+          'Fundo voltado para investimentos em ações de alta performance',
+        cnpj_gestor_fundo: '98765432000188',
+        nome_backoffice: 'Backoffice Alpha',
+        email_backoffice: 'backoffice.alpha@empresa.com',
+        telefone_backoffice: '1122334455',
+        nome_administrador: 'Administrador Alpha',
+        email_administrador: 'admin.alpha@empresa.com',
+        cnpj_administrador: '11223344000166',
+        telefone_administrador: '11987654321',
+        nome_representante: 'Representante Alpha',
+        cpf_representante: '12345678901',
+        email_representante: 'representante.alpha@empresa.com',
+        telefone_representante: '11987654322',
+        cep_endereco_representante: '12345678',
+        rua_endereco_representante: 'Rua Exemplo',
+        numero_endereco_representante: '1000',
+        bairro_endereco_representante: 'Centro',
+        municipio_endereco_representante: 'São Paulo',
+        estado_endereco_representante: 'SP',
+        codigo_banco: '001',
+        agencia_banco: '1234',
+        conta_banco: '56789-0',
+        faturamento_anual: '1000000',
+      },
+    ],
+  })
+  fundos: CreateFundoDto[];
 }
