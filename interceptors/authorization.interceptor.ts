2f5ad1340e62727e516d733eb4e282dae1d11c7d
Thalys Silva <thalys.consultoria@wefin.com.br>
2024-10-17 21:30:29 -0300

    try: entendendo fluxo de autenticação

--- interceptors/authorization.interceptor.ts
+++ interceptors/authorization.interceptor.ts
@@ -11,8 +11,6 @@ import { jwtConstants } from 'src/app/auth/constants';
 @Injectable()
 export class AuthorizationInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('interceptando...');
-    console.log(context.getArgs());
     const request = context.switchToHttp().getRequest();
     const response = context.switchToHttp().getResponse();
     const segTimeBeforeExpiration =
