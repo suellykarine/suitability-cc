be6d093b25af501ac0a1e36e62ef09e53cbfa119
Henrique Pires Bezerra <hpb.consultoria@wefin.com.br>
2024-10-21 18:12:32 -0300

    chore: retirando laqusController da appModules e organizando imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,58 +1,57 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './app/auth/auth.module';
-import { UsuariosModule } from './app/usuarios/usuario.module';
+import { EstruturacaoCarrinhoModule } from './app/estruturacao-carrinho/estruturacao-carrinho.module';
+import { AuthorizationInterceptor } from 'interceptors/authorization.interceptor';
+import { AnalisePerfilModule } from './app/analise-perfil/analise-perfil.module';
+import { StatusUsuarioModule } from './app/status-usuario/status-usuario.module';
+import { BuscarArquivoModule } from './app/buscar-arquivo/buscar-arquivo.module';
 import { CartaConviteModule } from './app/carta-convite/carta-convite.module';
 import { PreRegistroModule } from './app/pre-registro/pre-registro.module';
-import { AuthorizationInterceptor } from 'interceptors/authorization.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LaqusController } from './laqus/laqus.controller';
-import { LaqusModule } from './laqus/laqus.module';
-import { SrmBankModule } from './srm-bank/srm-bank.module';
-import { AtivosModule } from './app/ativos/ativos.module';
-import { PdfModule } from './app/pdf/pdf.module';
-import { PainelModule } from './app/painel/painel.module';
 import { EnviarEmailModule } from './app/enviar-email/enviar-email.module';
-import { FundosModule } from './app/fundos/fundos.module';
-import { DocumentosModule } from './app/documentos/documentos.module';
-import { AdmModule } from './app/adm/adm.module';
-import { CedenteModule } from './app/cedente/cedente.module';
-import { CcbModule } from './app/ccb/ccb.module';
-import { StatusUsuarioModule } from './app/status-usuario/status-usuario.module';
-import { EstruturacaoCarrinhoModule } from './app/estruturacao-carrinho/estruturacao-carrinho.module';
-import { AnalisePerfilModule } from './app/analise-perfil/analise-perfil.module';
-import { FeedbackModule } from './app/feedback/feedback.module';
-import { BuscarArquivoModule } from './app/buscar-arquivo/buscar-arquivo.module';
 import { DebenturesModule } from './app/debentures/debentures.module';
+import { DocumentosModule } from './app/documentos/documentos.module';
+import { FeedbackModule } from './app/feedback/feedback.module';
+import { UsuariosModule } from './app/usuarios/usuario.module';
+import { CedenteModule } from './app/cedente/cedente.module';
+import { SrmBankModule } from './srm-bank/srm-bank.module';
 import { AmbimaModule } from './app/ambima/anbima.module';
+import { PainelModule } from './app/painel/painel.module';
+import { FundosModule } from './app/fundos/fundos.module';
+import { AtivosModule } from './app/ativos/ativos.module';
+import { LaqusModule } from './app/laqus/laqus.module';
+import { AuthModule } from './app/auth/auth.module';
+import { CcbModule } from './app/ccb/ccb.module';
+import { AppController } from './app.controller';
+import { AdmModule } from './app/adm/adm.module';
+import { PdfModule } from './app/pdf/pdf.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
 
 @Module({
   imports: [
-    AuthModule,
-    UsuariosModule,
+    EstruturacaoCarrinhoModule,
+    AnalisePerfilModule,
+    BuscarArquivoModule,
+    StatusUsuarioModule,
     CartaConviteModule,
     PreRegistroModule,
-    AtivosModule,
-    PdfModule,
-    PainelModule,
     EnviarEmailModule,
-    FundosModule,
-    DocumentosModule,
-    AdmModule,
-    CedenteModule,
-    CcbModule,
-    StatusUsuarioModule,
-    EstruturacaoCarrinhoModule,
-    LaqusModule,
-    SrmBankModule,
-    AnalisePerfilModule,
-    FeedbackModule,
-    BuscarArquivoModule,
     DebenturesModule,
+    DocumentosModule,
+    UsuariosModule,
+    FeedbackModule,
+    SrmBankModule,
+    CedenteModule,
+    AtivosModule,
+    PainelModule,
+    FundosModule,
     AmbimaModule,
+    LaqusModule,
+    AuthModule,
+    CcbModule,
+    PdfModule,
+    AdmModule,
   ],
-  controllers: [AppController, LaqusController],
+  controllers: [AppController],
   providers: [
     AppService,
     {
