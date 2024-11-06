9221b5441ff86d80ee34d5619867a69853db61a8
lorenzo.consultoria+1-at-887626648304 <lorenzo.consultoria@wefin.com.br>
2024-11-06 10:18:12 -0300

    fix:token renovacao

--- src/app/autenticacao/constants.ts
+++ src/app/autenticacao/constants.ts
@@ -1,7 +1,7 @@
 export const jwtConstants = {
   secret: process.env.TOKEN_KEY,
   sercretPreRegister: process.env.TOKEN_PRE_REGISTER,
-  secretTokenAtualizacao: process.env.TOKEN_ATUALIZACAO_KEY,
+  secretTokenAtualizacao: process.env.TOKEN_RENOVACAO_KEY,
 };
 
 export const sigmaHeaders = {
