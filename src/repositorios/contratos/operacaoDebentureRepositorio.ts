771ccecda8f2f416433db97a48fb2c1921001bb8
suelly.consultoria-at-887626648304 <suelly.consultoria@wefin.com.br>
2024-11-27 11:41:20 -0300

    fix: importação na operacaoDebenture

--- src/repositorios/contratos/operacaoDebentureRepositorio.ts
+++ src/repositorios/contratos/operacaoDebentureRepositorio.ts
@@ -1,4 +1,4 @@
-import { OperacaoDebenture } from 'src/@types/entities/operacaoDEbenture';
+import { OperacaoDebenture } from 'src/@types/entities/operacaoDebenture';
 
 export abstract class OperacaoDebentureRepositorio {
   abstract buscarPorFundoInvestimento(id: number): Promise<OperacaoDebenture[]>;
