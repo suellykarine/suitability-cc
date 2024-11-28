c31533c7273a54c776d7febf09cb9f5d2539e0de
hpb.consultoria <hpb.consultoria@wefin.com.br>
2024-11-28 12:03:14 -0300

    fix: corrigindo o SrmBank maiusculo que gerava um erro de importacao

--- src/app/srm-bank/srm-bank.controller.ts
+++ src/app/srm-bank/srm-bank.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Post, Body, UseGuards, Param, Get } from '@nestjs/common';
-import { SrmBankService } from './srm-bank.service';
 import { JwtAuthGuardBackoffice } from 'src/app/autenticacao/guards/backoffice-auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CriarContaSrmBankDto } from './dto/criar-conta-srm-bank.dto';
 import { JwtAuthGuardPremium } from '../autenticacao/guards/premium-auth.guard';
+import { SrmBankService } from './srm-bank.service';
 
 @ApiTags('SRM-bank')
 @ApiBearerAuth('access-token')
 @Controller('api/srm-bank')
 export class SrmBankController {
-  constructor(private readonly SrmBankService: SrmBankService) {}
+  constructor(private readonly srmBankService: SrmBankService) {}
 
   @UseGuards(JwtAuthGuardBackoffice)
   @Post(':id_cedente')
@@ -17,7 +17,7 @@ export class SrmBankController {
     @Body() criarConta: CriarContaSrmBankDto,
     @Param('id_cedente') id_cedente: string,
   ) {
-    return this.SrmBankService.criarContaInvestidor({
+    return this.srmBankService.criarContaInvestidor({
       identificador: criarConta.identificador,
       id_cedente,
     });
@@ -27,7 +27,7 @@ export class SrmBankController {
   buscarContaInvestidor(
     @Param('id_fundo_investidor') id_fundo_investidor: string,
   ) {
-    return this.SrmBankService.buscarContaInvestidor(
+    return this.srmBankService.buscarContaInvestidor(
       Number(id_fundo_investidor),
     );
   }
