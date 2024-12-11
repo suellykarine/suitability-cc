a0a02109266e9431ec93af3a25d19f4d9b672ec2
suelly.consultoria-at-887626648304 <suelly.consultoria@wefin.com.br>
2024-12-11 16:49:19 -0300

    alteraçoes na rota da operacoesInvest

--- src/app/operacoes-invest/operacoes-invest.service.ts
+++ src/app/operacoes-invest/operacoes-invest.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ErroAplicacao, ErroServidorInterno } from 'src/helpers/erroAplicacao';
 
 @Injectable()
 export class OperacoesInvestService {
@@ -31,11 +32,16 @@ export class OperacoesInvestService {
     }
   }
 
-  async buscarTodasOperacoes() {
+  async buscarTodasOperacoes(identificadorInvestidor: string) {
     try {
-      const url = `${process.env.BASE_URL_OPERACAO_INVEST}`;
-
-      const req = await fetch(url, {
+      const credictConnectCedente = process.env.IDENTIFICADOR_CEDENTE;
+      const url = `${process.env.BASE_URL_OPERACAO_INVEST}?identificadorCedente=${credictConnectCedente}`;
+      const urlMontada =
+        url +
+        (identificadorInvestidor
+          ? `?identificadorInvestidor=${identificadorInvestidor}`
+          : '');
+      const req = await fetch(urlMontada, {
         headers: {
           'Content-Type': 'application/json',
           'X-API-KEY': process.env.X_API_KEY,
@@ -43,17 +49,23 @@ export class OperacoesInvestService {
       });
 
       if (!req.ok) {
-        throw new InternalServerErrorException(
-          'Ocorreu um erro ao buscar as operações',
-        );
+        const resposta = await req.json();
+        throw new ErroServidorInterno({
+          mensagem: 'Ocorreu um erro ao buscar as operações',
+          acao: 'operacaoInvestService.buscarTodasOperacoes',
+          informacaoAdicional: { req, resposta },
+        });
       }
 
       const result = await req.json();
       return result;
     } catch (error) {
-      throw new InternalServerErrorException(
-        'Ocorreu um erro ao buscar as operações',
-      );
+      if (error instanceof ErroAplicacao) throw error;
+      throw new ErroServidorInterno({
+        mensagem: 'Ocorreu um erro ao buscar as operações',
+        acao: 'operacaoInvestService.buscarTodasOperacoes',
+        informacaoAdicional: { error },
+      });
     }
   }
 }
