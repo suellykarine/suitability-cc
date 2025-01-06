dece8f76473dc136cf2b3b37ba5c1e60e22cfe9f
hpb.consultoria <hpb.consultoria@wefin.com.br>
2025-01-06 12:45:43 -0300

    feat:adicionando logs na pagina de ativos

--- src/app/ativos/ativos.service.ts
+++ src/app/ativos/ativos.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { QueryDto } from './dto/query-ativos.dto';
 import { Ativo } from './entities/ativo.entity';
 import { sigmaHeaders } from '../autenticacao/constants';
+import { tratarErroRequisicao } from 'src/utils/funcoes/erros';
 
 @Injectable()
 export class AtivosService {
@@ -32,16 +33,28 @@ export class AtivosService {
 
     let itensSigma: Ativo[] = [];
     const urlSigmaParaObterTotalItens = this.construirUrlComFiltros(params);
-    const sigmaFetch = await fetch(urlSigmaParaObterTotalItens, options);
-    const data = await sigmaFetch.json();
+    const req = await fetch(urlSigmaParaObterTotalItens, options);
+    if (!req.ok) {
+      await tratarErroRequisicao({
+        acao: 'AtivosService.encontrarTodosAtivos',
+        mensagem: 'Erro ao buscar os ativos',
+        req,
+        detalhes: {
+          status: req.status,
+          texto: req.statusText,
+          url: req.url,
+        },
+      });
+    }
+    const data = await req.json();
     itensSigma = data.content;
 
     const ativosComDadosEmpresa = await Promise.all(
       itensSigma.map(async (ativo: any) => {
         const identificacaoAtivo = ativo.cedente.identificacao;
         const urlEmpresaSigma = `${process.env.ATIVOS_URL}/ativos-disponiveis/v1/ativos/cedente/${identificacaoAtivo}`;
-        const empresaSigma = await fetch(urlEmpresaSigma, options);
-        const dadosEmpresaSigma = await empresaSigma.json();
+        const reqEmpresaSigma = await fetch(urlEmpresaSigma, options);
+        const dadosEmpresaSigma = await reqEmpresaSigma.json();
         return { ...ativo, empresa: { ...dadosEmpresaSigma } };
       }),
     );
@@ -49,7 +62,7 @@ export class AtivosService {
     const itens = ativosComDadosEmpresa
       .filter((ativo) => ativo.tir > 0 && ativo.cedente.scoreInterno !== 'F')
       .map((ativo) => {
-        let novoAtivo = ativo;
+        const novoAtivo = ativo;
         switch (ativo.cedente.scoreInterno) {
           case 'B':
             novoAtivo.cedente.scoreInterno = 'A';
@@ -87,8 +100,20 @@ export class AtivosService {
 
     const urlSigma = `${process.env.ATIVOS_URL}/ativos-disponiveis/v1/ativos/geral`;
 
-    const sigmaFetch = await fetch(urlSigma, options);
-    const data = await sigmaFetch.json();
+    const req = await fetch(urlSigma, options);
+    if (!req.ok) {
+      await tratarErroRequisicao({
+        acao: 'AtivosService.geral',
+        mensagem: 'Erro ao buscar os ativos',
+        req,
+        detalhes: {
+          status: req.status,
+          texto: req.statusText,
+          url: req.url,
+        },
+      });
+    }
+    const data = await req.json();
 
     return data;
   }
